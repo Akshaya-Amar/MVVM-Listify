4285d2794999a70ad36ad40b5263a48771196af2
akshayaamar <akshayaamar05@gmail.com>
2025-03-28 18:20:10 +0530

    removed unused code

--- app/src/main/java/com/example/mvvmretrofitkotlin/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/mvvmretrofitkotlin/data/repository/UserRepository.kt
@@ -25,30 +25,3 @@ class UserRepository(
         }
     }
 }
-
-/*which way is better
-1st to return the data as Result<>
-suspend fun fetchUsers(): Result<UserData> = withContext(Dispatchers.IO) {
-        try {
-            val response = apiService.getUsers()
-            if (response.isSuccessful) {
-                response.body()?.let { data ->
-                    return@withContext Result.Success(data)
-                } ?: run {
-                    return@withContext Result.Failure(Exception(response.message()))
-                }
-            } else {
-                return@withContext Result.Failure(Exception("Not successful: ${response.code()}, ${response.message()}"))
-            }
-        } catch (exception: Exception) {
-            return@withContext Result.Failure(exception)
-        }
-    }
-
-or
-2nd one to handle using success and failure callbacks in arguments?
-
-which is better and recommended?
-
-ChatGPT said:
-​*/
